3d949e25546b8f909f30e188f350f6216e892216
Eamonn de Leastar <edeleastar@wit.ie>
2025-10-14 11:10:42 +0100

    support tutorial in walls

--- cli/tutors-model-lib/src/utils/course-utils.ts
+++ cli/tutors-model-lib/src/utils/course-utils.ts
@@ -96,7 +96,7 @@ export function createCompanions(course: Course) {
 export function createWalls(course: Course) {
   course.walls = [];
   course.wallMap = new Map<string, Lo[]>();
-  ["talk", "note", "lab", "web", "archive", "github"].forEach((type) => addWall(course, type as LoType));
+  ["talk", "tutorial", "note", "lab", "web", "archive", "github"].forEach((type) => addWall(course, type as LoType));
   course.wallBar = {
     show: true,
     bar: [],
