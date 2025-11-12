e9ab96a9272adf3845afc736acd9c480ca020466
Eamonn de Leastar <edeleastar@wit.ie>
2025-11-12 14:01:51 +0100

    correct type

--- cli/tutors-model-lib/src/utils/course-utils.ts
+++ cli/tutors-model-lib/src/utils/course-utils.ts
@@ -150,7 +150,7 @@ export function loadPropertyFlags(course: Course) {
   }
   course.hasWhiteList = false;
   if (course.properties?.whitelist) {
-    course.hasWhiteList = course.properties?.whitelist === "1";
+    course.hasWhiteList = course.properties?.whitelist as unknown as number === 1;
   }
   course.ignorePin = course.properties?.ignorepin?.toString();
   if (course.properties?.icon && typeof course.properties.icon === "object") {
