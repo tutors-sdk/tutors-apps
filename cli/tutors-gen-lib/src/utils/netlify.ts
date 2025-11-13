3286da90686e28935316f3ffee0a4f7e8eb5d68d
Eamonn de Leastar <edeleastar@wit.ie>
2025-11-13 07:57:13 +0000

    referse update

--- cli/tutors-gen-lib/src/utils/netlify.ts
+++ cli/tutors-gen-lib/src/utils/netlify.ts
@@ -38,6 +38,7 @@ function redirectHtmlFile(): string {
       <body>
         <script>
           const host = window.location.host;
+          const subdomain = host.split('.')[0];
           window.location = "https://tutors.dev/course/" + subdomain;
         </script>
       </body>
